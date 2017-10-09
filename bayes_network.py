1c737ab665daba2bb0bb2c59be7a6950f7a48648
EduardoVaca <vacalalo@gmail.com>
2017-10-09 17:35:54 -0500

    Add first parser for node names

---
+++ bayes_network.py
@@ -0,0 +1,16 @@
+
+
+class BayesNode:
+
+    def __init__(self, name):
+        self.name = name
+
+def read_nodes():
+    while True:
+        current_input = input()
+        if current_input == '[Nodes]':
+            break
+    return [BayesNode(x) for x in input().replace(' ', '').split(',')]    
+
+nodes = read_nodes()
+print(nodes)
