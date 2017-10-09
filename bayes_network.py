b10f1074832ef5c32980487cfa61509345e004da
EduardoVaca <vacalalo@gmail.com>
2017-10-09 18:17:46 -0500

    Add parse methods for adding probabilities

--- bayes_network.py
+++ bayes_network.py
@@ -6,10 +6,17 @@ class BayesNode:
 
     def __init__(self, name):
         self.name = name
+        self.parents = []
+        self.probabilities = {}
 
     def __str__(self):
-        return self.name
+        return self.name + '\nParents: ' + ''.join([x.name for x in self.parents]) + '\n' + str(self.probabilities)
 
+    def add_probability(self, parents_cond, parents, prob):
+        """Adds probability value to probability table"""
+        self.parents += [parent for parent in parents if parent not in self.parents]
+        self.probabilities[self.name+''.join(parents_cond)] = prob
+        
 class BayesNetwork:
     """Class that represents a Bayes Network
     """
@@ -17,6 +24,11 @@ class BayesNetwork:
     def __init__(self, node_names):
         self.nodes = [BayesNode(name) for name in node_names]
         self.nodes_index = {node.name: i for i, node in enumerate(self.nodes)}
+
+    def add_probability_to_node(self, node_name, parents_cond, prob):
+        """Adds probability values to one of its nodes
+        """
+        self.nodes[self.nodes_index[node_name]].add_probability(parents_cond, [self.nodes[self.nodes_index[x[1:]]] for x in parents_cond], prob)
 
 def read_nodes():
     """Parser for reading node names
@@ -29,7 +41,7 @@ def read_nodes():
             break
     return BayesNetwork(input().replace(' ', '').split(','))
 
-def read_probability_tables():
+def read_probability_tables(bayes_net):
     """Parser for prob tables, it adds tables to the nodes
     """
     while True:
@@ -40,13 +52,18 @@ def read_probability_tables():
         probability = input()
         if probability == '':
             break
-
+        values = probability.replace(' ', '').split('=')
+        var_assignments = values[0].split('|')
+        parents_cond = [] if len(var_assignments) <= 1 else var_assignments[1].split(',')
+        bayes_net.add_probability_to_node(var_assignments[0][1:], parents_cond, values[1])
 
 def main():
     """Main program
     """
     bayes_net = read_nodes()
-    print(bayes_net.nodes_index)
+    read_probability_tables(bayes_net)
+    print(bayes_net.nodes[0])
+
 
 
 if __name__ == '__main__':
