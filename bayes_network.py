aa51461774dd6298f11bb6bb43c0975d9f7558b6
EduardoVaca <vacalalo@gmail.com>
2017-10-09 17:40:00 -0500

    Add comments

--- bayes_network.py
+++ bayes_network.py
@@ -1,6 +1,8 @@
 
 
 class BayesNode:
+    """Class that represents a node in the Bayes Network
+    """
 
     def __init__(self, name):
         self.name = name
@@ -9,11 +11,22 @@ class BayesNode:
         return self.name
 
 def read_nodes():
+    """Parser for reading node names
+    RETURNS:
+    - list of BayesNodes created
+    """
     while True:
         current_input = input()
         if current_input == '[Nodes]':
             break
     return [BayesNode(x) for x in input().replace(' ', '').split(',')]    
 
-nodes = read_nodes()
-print(nodes)
+def main():
+    """Main program
+    """
+    nodes = read_nodes()
+    print(nodes)
+
+
+if __name__ == '__main__':
+    main()
